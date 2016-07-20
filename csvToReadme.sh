173164a5dc5003ff3f061156e9420899d0421f82
anonymous <anonymous>
2016-07-20 11:18:46 +0200

    chore: add statz about files' contents

--- csvToReadme.sh
+++ csvToReadme.sh
@@ -11,37 +11,55 @@
 # ###### #
 
 README_FILE="README.md"
-CSV_LIB_FILES="Tips-n-tools_Libraries.csv"
-HTML_LIB_FILES="Tips-n-tools_Libraries.html"
-CSV_WEB_FILES="Tips-n-tools_WebLinks.csv"
-HTML_WEB_FILES="Tips-n-tools_WebLinks.html"
+
+CSV_LIB_FILE="Tips-n-tools_Libraries.csv"
+CSV_LIB_FILE_USELESS_ROWS=6
+HTML_LIB_FILE="Tips-n-tools_Libraries.html"
+
+CSV_WEB_FILE="Tips-n-tools_WebLinks.csv"
+CSV_WEB_FILE_USELESS_ROWS=6
+HTML_WEB_FILE="Tips-n-tools_WebLinks.html"
 
 README_HEADER="# Tips'n'tools";
+
 
 # ######### #
 # MAIN CODE #
 # ######### #
 
+# Check the args
 if [ "$#" -ne 0 ]; then
 	echo "Usage: sh csvToReadme.sh"
 	exit 0	
 fi
 
+# Update the README_FILE file
 echo "Write begin of readme file..."
 echo $README_HEADER > $README_FILE
 
 echo "Write HTML file from CSV about libraries..."
-cat $CSV_LIB_FILES | sh csvToHtml.sh --limitedHtml > $HTML_LIB_FILES
+cat $CSV_LIB_FILE | sh csvToHtml.sh --limitedHtml > $HTML_LIB_FILE
 
 echo "Write HTML file from CSV file about web links..."
-cat $CSV_WEB_FILES | sh csvToHtml.sh --limitedHtml > $HTML_WEB_FILES
+cat $CSV_WEB_FILE | sh csvToHtml.sh --limitedHtml > $HTML_WEB_FILE
 
 echo "Write README.md with HTML files' contents"
 echo "\n\n" >> $README_FILE
-echo "__" $CSV_LIB_FILES "__" >> $README_FILE
-cat $HTML_LIB_FILES >> $README_FILE
+echo "__" $CSV_LIB_FILE "__" >> $README_FILE
+cat $HTML_LIB_FILE >> $README_FILE
 echo "\n\n" >> $README_FILE
-echo "__" $CSV_WEB_FILES "__" >> $README_FILE
-cat $HTML_WEB_FILES >> $README_FILE
+echo "__" $CSV_WEB_FILE "__" >> $README_FILE
+cat $HTML_WEB_FILE >> $README_FILE
 
+# Some stats about the number of fields
+csvLibsRows=`cat $CSV_LIB_FILE | wc -l`
+csvWebRows=`cat $CSV_WEB_FILE | wc -l`
+csvLibsRowsCleaned=$(($csvLibsRows - $CSV_LIB_FILE_USELESS_ROWS))
+csvWebRowsCleaned=$(($csvWebRows - $CSV_WEB_FILE_USELESS_ROWS))
+
+echo "Now we have $csvLibsRowsCleaned items in $CSV_LIB_FILE"
+echo "Now we have $csvWebRowsCleaned items in $CSV_WEB_FILE"
+
+# Finish!
 echo "Terminated ! ~=[,,_,,]:3"
+
